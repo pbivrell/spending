97f66ab0a6b6daf8f2db383fe11ae0c08c61b38c
Paul Bivrell <prbivrell@gmail.com>
2021-08-29 14:39:54 -0600

    Fixed refresh url

--- client/src/components/inMemoryJWT.js
+++ client/src/components/inMemoryJWT.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const url = 'http://localhost:8080/api/v1/refresh'
-const url = 'http://spend-it-gatekeeper:8080/api/v1/refresh'
+const url = 'http://spend-it-gatekeeper/api/v1/refresh'
 
 const inMemoryJWTManager = () => {
     let logoutEventName = 'ra-logout';
